d88bc57a25e5575245bba1e078ce268ea93dc030
Xu Zhiwei <1027660817@qq.com>
2022-04-02 14:27:12 +0800

    Update install_sc2.sh

--- install_sc2.sh
+++ install_sc2.sh
@@ -17,9 +17,9 @@ echo 'SC2PATH is set to '$SC2PATH
 
 if [ ! -d $SC2PATH ]; then
         echo 'StarCraftII is not installed. Installing now ...';
-        wget http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.10.zip
-        unzip -P iagreetotheeula SC2.4.10.zip
-        rm -rf SC2.4.10.zip
+        wget -c http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.6.2.69232.zip
+        unzip -P iagreetotheeula SC2.4.6.2.69232.zip
+        rm -rf SC2.4.6.2.69232.zip
 else
         echo 'StarCraftII is already installed.'
 fi
@@ -39,4 +39,3 @@ mv SMAC_Maps $MAP_DIR
 rm -rf SMAC_Maps.zip
 
 echo 'StarCraft II and SMAC are installed.'
-
